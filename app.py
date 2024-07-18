88c6f555bc2ececafc6f471a77d59ab166e54e0c
Manuel Folgar <manuel.folgarberrios@csedge.org>
2024-07-18 13:29:29 -0700

    app.py completed

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-import random
+from flask import Flask, render_template, request
 
-from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def madlib_completed():
+    return render_template('madlib.html')
+
+@app.route('/completed', methods=['POST'])
+def fill_in_black():
+    noun = request.form['noun']
+    verb = request.form['verb']
+    adverb = request.form['adverb']
+    adjective = request.form['adjective']
+    place = request.form['name of place']
+
+    madlib_story = f"In a sleepy village called {place} nestled by a tranquil river, lived a curious cat named Whiskers. He loved to {verb} {adverb} through the place, chasing {noun}. One day, he discovered a {adjective} {noun} tucked beneath an old {noun}. With a {adjective}  {verb}, he {verb} it and {adverb} dashed home, where its {adjective} magic brought joy to everyone in the village"
 
 
+
+
+    
+    return render_template('completed.html', madlib_story=madlib_story)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
